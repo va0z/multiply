7b13dec722639e1eff5365854d2d860458d5cc71
va0z <va0z@ya.ru>
2019-09-26 21:40:55 +1000

    complit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let firstBig = BigInt(first);
+  let secondBig = BigInt(second);
+  let proiz = firstBig * secondBig;
+  let res = String(proiz);
+  
+  // console.log(res);
+  return res;
 }
